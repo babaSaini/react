7b2f1397cad839bd3e150c1136a902018dd124f7
babaSaini <151121147+babaSaini@users.noreply.github.com>
2023-11-20 18:50:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<>
+<div class="main">
+<div className="center-container">
+            <div>
+                <div className="rate">
+                    <div className="rate_num">
+                        <p>4.9</p>
+                    </div>
+                    <div>
+                        <img src="Group 113.png" alt="error!!!!!!!" className="rate_star"/>
+                    </div>
+                </div>
+                <div className="rate_text">
+                    <p>(106 Ratings & Reviews)</p>
+                    <h3>(360) 623-9035</h3>
+                </div>
+            </div>
+            <div className="logo1">
+                <img src="logo.webp" alt="top_weather_logo"/>
+            </div>
+            <div className="button1">
+                <button>Request an Estimate</button>
+                <p className="link-button__text">Serving Thurston, Lewis, Mason, and Grays Harbor Counties, WA</p>
+            </div>
+        </div>
+        </div>
+</>
+);
+}
+
+                      export default App;
